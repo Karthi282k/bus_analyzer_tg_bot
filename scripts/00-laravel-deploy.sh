ac880ff3acd29021d751ec9cd665efc19cae4d98
Karthi282k <karthigaiselvan28062000@gmail.com>
2024-01-22 15:35:34 +0530

    Minor Changes in scripr file

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -5,7 +5,7 @@ echo "Running composer"
 composer self-update 2.1.14
 composer dump-autoload
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html --ignore-platform-reqs
+composer install --no-dev --working-dir=/var/www/html --no-scripts
 
 echo "generating application key..."
 php artisan key:generate --show
