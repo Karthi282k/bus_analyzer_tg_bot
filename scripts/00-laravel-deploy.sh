6c26282cdc4a2e423024bf76b08fe2c8edfeac95
Karthi282k <karthigaiselvan28062000@gmail.com>
2024-01-22 14:49:37 +0530

    Script changes02

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ echo "Running composer"
 composer self-update 2.1.14
 composer dump-autoload
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html --ignore-platform-reqs
 
 echo "generating application key..."
 php artisan key:generate --show
